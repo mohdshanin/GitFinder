bc32801f950572a0e3b2bd3905f8f869b4445680
Mohd Shan <shansid2000@gmail.com>
2021-04-25 17:55:42 +0530

    commit i

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Search from './components/layout/Search';
 import Footer from './components/layout/Footer';
-import Users from './components/users/Users';
+
 import User from './components/users/User';
 import About from './components/pages/About';
-// import Home from './components/pages/Home';
+import Home from './components/pages/Home';
 import './sass/App.css';
 
 import GithubState from './context/github/GithubState';
@@ -20,20 +19,9 @@ const App = () => {
           <Navbar />
           <div>
             <Switch>
-              <Route
-                exact
-                path="/"
-                render={() => {
-                  return (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  );
-                }}
-              />
               <Route exact path="/user/:login" component={User} />
               <Route exact path="/about" component={About} />
+              <Route exact path="/" component={Home} />
             </Switch>
           </div>
           <Footer />
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React, { Fragment } from 'react';
+import Search from './components/layout/Search';
+import Users from './components/users/Users';
 
 const Home = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <Search />
+      <Users />
+    </Fragment>
+  );
 };
 
 export default Home;
